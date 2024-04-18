63211d965994ce50ac9e12ef13672814ff3c478e
Pepperzero <chimichanga91@hotmail.it>
2024-04-18 18:47:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-alert("it works");
+//alert("does it?");
+
+var swiper = new Swiper(".swiper.is-slider-thumb", {
+  spaceBetween: 12,
+  slidesPerView: 5,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+var swiper2 = new Swiper(".is-slider-centro", {
+  spaceBetween: 12,
+  autoplay: {
+    delay: 5000,
+  },
+  /*
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        */
+  thumbs: {
+    swiper: swiper,
+  },
+});
