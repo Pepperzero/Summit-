0223654bed2985358da2faa0890069394ded9ff9
Pepperzero <chimichanga91@hotmail.it>
2024-04-22 09:49:14 +0200

    update index.js

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
-//alert("does it?");
+//alert(" it?");
 
 var swiper = new Swiper(".swiper.is-slider-thumb", {
   spaceBetween: 12,
   slidesPerView: 5,
+  centerInsufficientSlides: true,
   freeMode: true,
   watchSlidesProgress: true,
 });
 
-var swiper2 = new Swiper(".is-slider-centro", {
+var swiper2 = new Swiper(".swiper.is-slider-centro", {
   spaceBetween: 12,
   autoplay: {
     delay: 5000,
   },
-  /*
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-        */
   thumbs: {
     swiper: swiper,
   },
 });
+
+var swiper3 = new Swiper(".swiper.is-slider-related-posts", {
+  spaceBetween: 12,
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+  centerInsufficientSlides: true,
+  //loop: true,
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+    disabledClass: "is-disabled",
+  },
+});
+
+/*
+navigation: {
+        nextEl: $(this).find(".swiper-next")[0],
+        prevEl: $(this).find(".swiper-prev")[0],
+        disabledClass: "is-disabled"
+      },
+*/
